ee1bf0d18dd0d122e46f98f5067a8917a8ccb56d
medev21 <mebenav88@gmail.com>
2015-07-09 18:26:30 -0500

    add one comment

--- simpleSymbols/app.js
+++ simpleSymbols/app.js
@@ -1,6 +1,9 @@
 function change(){
 		str= document.getElementById("myInput1").value;
+		str = str.split('');
+		strLen = str.length;
 		
+		//for loop; 
 		document.getElementById("second").innerHTML = "<p>" + result +"</p>";
 	
 }
