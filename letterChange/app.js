3bab01e3756fefa6d443942b77d47f0cdb3ad37a
medev21 <mebenav88@gmail.com>
2015-07-08 21:52:14 -0500

    modified app.js: add for loop to uppercase vowels, only the current word; modified stuff.html: add input tag

--- letterChange/app.js
+++ letterChange/app.js
@@ -1,26 +1,49 @@
 function change(){
-		str = "hello*3";
-		word = str.toLowerCase();
-		wordLen = str.length;
-		result = [];
-		console.log(String.fromCharCode(word.charCodeAt(6)))
-		for(i = 0; i < wordLen; i++){
-			if(word[i] == ' '){
-				result.push(' ');
-			}
-			else if(!isNaN(parseInt(word.charCodeAt(i)))){
-				result.push(String.fromCharCode(word.charCodeAt(i)+1));
-				console.log(result);
-			}
-			// else{
-				
+		//str= document.getElementById("myInput1").value;
+		str= "hello World*3!!";
+		// word = str.toLowerCase();
+		// wordLen = str.length;
+		// result = [];
+		// console.log(String.fromCharCode(word.charCodeAt(6)))
+		// for(i = 0; i < wordLen; i++){
+			// if(word[i] == ' '){
+				// result.push(' ');
 			// }
-		}
-		result = result.join("")
+			// else if(!isNaN(parseInt(word.charCodeAt(i)))){
+				// result.push(String.fromCharCode(word.charCodeAt(i)+1));
+				// console.log(result);
+			// }
+			// // else{
+				
+			// // }
+		// }
+		// result = result.join("")
 		// result = str.replace(str[0], alpha[1]);
 		// result = str.replace(str[1], alpha[24]);
-		document.getElementById("first").innerHTML = "<p>" + str +"</p>";
-		document.getElementById("second").innerHTML = "<p>" + result +"</p>";
+		
+		str = str.toLowerCase();
+		str = str.match(/\w+|\W+/g);
+		//str = str.match(/[a-z]+|[0-9]+/g);
+		console.log(str);
+		
+		for(i = 0; i < str.length; i++){
+			if(typeof str[i] == 'string' && str[i] != ' '){
+				console.log(str[i]);
+			}
+		}
+		
+		// str = str.toLowerCase();
+		// str = str.split('');
+		// strCount = str.length;
+		// console.log(str);
+		// for(var i = 0; i < strCount; i++){
+			// if( str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ){
+				// str[i] = str[i].toUpperCase();
+				// console.log(str[i])
+			// }
+		// }
+		// str = str.join('');
+		document.getElementById("second").innerHTML = "<p>" + str +"</p>";
 	
 }
 	
