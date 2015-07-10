66ea27b8bc0993fe35df1f74c977f029788125b1
medev21 <mebenav88@gmail.com>
2015-07-10 17:46:34 -0500

    add solution to simpleSymbols challenge

--- simpleSymbols/app.js
+++ simpleSymbols/app.js
@@ -1,9 +1,23 @@
 function change(){
 		str= document.getElementById("myInput1").value;
-		str = str.split('');
+		str = str.toLowerCase();
 		strLen = str.length;
 		
-		//for loop; 
+		for(i = 0; i < strLen; i++){
+			charLetter = str.charAt(i);
+			if(charLetter >= 'a' && charLetter<='z'){
+				prev = str.charAt(i-1);
+				next = str.charAt(i+1);
+				if(prev == '+' && next == '+'){
+					result = 'true';
+				}
+				else{
+					result = 'false';
+					break;
+				}
+			}
+		}
+		
 		document.getElementById("second").innerHTML = "<p>" + result +"</p>";
 	
 }
