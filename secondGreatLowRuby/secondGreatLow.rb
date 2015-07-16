6e08927f925663eaa65629b04cba18772b8334b3
medev21 <mebenav88@gmail.com>
2015-07-16 17:47:47 -0500

    add solution to secondGreatLow.rb challenge, shorter code!

--- secondGreatLowRuby/secondGreatLow.rb
+++ secondGreatLowRuby/secondGreatLow.rb
@@ -1,21 +1,15 @@
 def secondGreatLow(num)
-	sorted = num.sort		#if array values are unordered
-	low_val = ''
-	high_val = ''
 	
-	puts sorted.reverse.join(' ') if num.length == 2
+	uniqVals = num.sort.uniq
 	
-	sorted.each do |i|
-		if i != sorted[i + 1]
-			low_val = sorted[i + 1].to_s
-			puts low_val
-			puts low_val.class
-			break
-		end
-	end
-	
-	
+	puts uniqVals[0].to_s + ' ' + uniqVals[0].to_s if uniqVals.length ==1
+	puts uniqVals.reverse.join(' ').to_s if uniqVals.length == 2
+	puts uniqVals[1].to_s + ' ' + uniqVals[uniqVals.length - 2].to_s if uniqVals.length > 2
+
 end
 
 secondGreatLow([4, 90])
 secondGreatLow([1, 42, 42, 180])
+secondGreatLow([1, 2, 3, 100])
+secondGreatLow([1, 42, 42, 45,180])
+secondGreatLow([80, 80])
