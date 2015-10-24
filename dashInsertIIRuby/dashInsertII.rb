c2193c79df30fc68976a8148fe286f12d5c2c272
Martin Ben <mebenav88@gmail.com>
2015-10-24 18:33:02 -0400

    add each method to find odd and even number

---
+++ dashInsertIIRuby/dashInsertII.rb
@@ -0,0 +1,21 @@
+def DashInsertII(num)
+
+  # code goes here
+  number = num.to_s.split('')
+  str = ''
+  number.each_with_index do |value, key|
+    str = str + value
+    digit = value.to_i
+    next_digit = number[key+1].to_i
+    if (digit % 2 == 1) and (next_digit % 2 == 1)
+      str = str + '-'
+    elsif (digit % 2 == 0) and (next_digit % 2 == 0)
+      str = str + '*'
+    end
+  end
+  return str
+
+end
+
+puts DashInsertII(99946)
+# puts DashInsertII(56647304)
