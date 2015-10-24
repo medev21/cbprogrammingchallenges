fd094d182d4fd16f3282cf95d44bd30a6cba3a1e
Martin Ben <mebenav88@gmail.com>
2015-10-24 19:49:58 -0400

    change each to while for solution

--- dashInsertIIRuby/dashInsertII.rb
+++ dashInsertIIRuby/dashInsertII.rb
@@ -3,21 +3,28 @@ def DashInsertII(num)
   # code goes here
   number = num.to_s.split('')
   str = ''
-  number.each_with_index do |value, key|
+  key = 0
+  while key < number.length
+    value = number[key]
     str = str + value
     digit = value.to_i
     next_digit = number[key+1].to_i
-    if value != number.last
-      if (digit % 2 == 1) and (next_digit % 2 == 1)
+    if (digit != 0 && next_digit != 0)
+      if digit.odd? && next_digit.odd?
         str = str + '-'
+        puts "odd #{digit} , #{next_digit}"
+        puts str
       # elsif (digit % 2 == 0) and (next_digit % 2 == 0)
       #   str = str + '*'
-      elsif digit != 0 and next_digit != 0
-        if (digit % 2 == 0) and (next_digit % 2 == 0)
-          str = str + '*'
-        end
+      elsif digit.even? && next_digit.even?
+        str = str + '*'
+        puts "even #{digit} , #{next_digit}"
+        puts str
+      else
+        puts "nums #{digit} , #{next_digit}"
       end
     end
+    key += 1
   end
   return str
 
@@ -26,3 +33,5 @@ end
 puts DashInsertII(99946)
 puts DashInsertII(56647304)
 puts DashInsertII(56647340)
+puts DashInsertII(35566)
+puts DashInsertII(667488958374553)
