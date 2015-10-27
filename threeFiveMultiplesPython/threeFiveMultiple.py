51e5dd7183715169cbc70fae525b28564337d534
Martin Ben <mebenav88@gmail.com>
2015-10-27 10:04:31 -0400

    add solution

---
+++ threeFiveMultiplesPython/threeFiveMultiple.py
@@ -0,0 +1,21 @@
+def ThreeFiveMultiples(num):
+  # code goes here
+  sum = 0
+  i = 1
+  while i < num:
+      a = i * 3
+      b = i * 5
+
+      if a < num:
+          sum += a
+
+      if b < num:
+          sum += b
+
+      i += 1
+
+  return sum
+
+print(ThreeFiveMultiples(10))
+print(ThreeFiveMultiples(6))
+print(ThreeFiveMultiples(1))
