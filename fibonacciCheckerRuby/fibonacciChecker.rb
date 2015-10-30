3be9abe3e197e1e1e93db4f78bfc9e773ae09b40
Martin Ben <mebenav88@gmail.com>
2015-10-29 22:57:26 -0400

    add solution to fibonacci

--- fibonacciCheckerRuby/fibonacciChecker.rb
+++ fibonacciCheckerRuby/fibonacciChecker.rb
@@ -2,8 +2,21 @@ def FibonacciChecker(num)
 
   # code goes here
   return 'yes' if num == 0 or num == 1
-  return num
 
+  i = 3
+  fib_1 = 2
+  fib_2 = 1
+  while i < num
+    fib = fib_1 + fib_2
+    if fib == num
+      return 'yes'
+    else
+      fib_2 = fib_1
+      fib_1 = fib
+      i += 1
+    end
+  end
+  return 'no'
 end
 
-puts FibonacciChecker(0)
+puts FibonacciChecker(54)
