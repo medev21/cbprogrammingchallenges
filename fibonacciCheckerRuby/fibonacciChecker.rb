fbb83e5f3fc31110ca41c2dd28e77c5ea46f3efa
Martin Ben <mebenav88@gmail.com>
2015-10-29 23:08:12 -0400

    clean up code

--- fibonacciCheckerRuby/fibonacciChecker.rb
+++ fibonacciCheckerRuby/fibonacciChecker.rb
@@ -1,22 +1,24 @@
 def FibonacciChecker(num)
-
-  # code goes here
+  i = 0
+  fib_1 = 1
+  fib_2 = 0
   return 'yes' if num == 0 or num == 1
 
-  i = 3
-  fib_1 = 2
-  fib_2 = 1
-  while i < num
+  while i <= num
     fib = fib_1 + fib_2
-    if fib == num
-      return 'yes'
-    else
-      fib_2 = fib_1
-      fib_1 = fib
-      i += 1
-    end
+    return 'yes' if fib == num
+    fib_2 = fib_1
+    fib_1 = fib
+    i += 1
   end
+  
   return 'no'
 end
 
 puts FibonacciChecker(54)
+puts FibonacciChecker(34)
+puts FibonacciChecker(8)
+puts FibonacciChecker(0)
+puts FibonacciChecker(1)
+puts FibonacciChecker(3)
+puts FibonacciChecker(2)
