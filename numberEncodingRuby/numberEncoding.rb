d6ca26b47cd5d16853b68f8be53265ab4e935ca0
Martin Ben <mebenav88@gmail.com>
2015-10-31 18:51:35 -0400

    add new solution in older ruby version 1.8

--- numberEncodingRuby/numberEncoding.rb
+++ numberEncodingRuby/numberEncoding.rb
@@ -1,17 +1,32 @@
 def NumberEncoding(str)
 
   # code goes here
-  arr = []
+  # # version ruby 2.2.3
+  # arr = []
+  #
+  # str.split('').each do |c|
+  #   if c.ord > 96
+  #     arr << (c.ord - 96)
+  #   else
+  #     arr << c
+  #   end
+  # end
+  # return arr.join()
 
-  str.split('').each do |c|
-    if c.ord > 96
-      arr << (c.ord - 96)
+  ######################
+  arr = []
+  str.downcase.each_byte do |c|
+    if c > 96
+      arr << (c - 96)
     else
-      arr << c
+      arr << (c.chr)
     end
   end
+
   return arr.join()
 
 end
 
 puts NumberEncoding("hello 45")
+puts NumberEncoding("jaj-a")
+puts NumberEncoding("af5c a#!")
