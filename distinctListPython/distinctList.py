26c47c6b7a107de32a5dd0e9b55c0aef706a64ad
Martin Ben <mebenav88@gmail.com>
2015-11-02 23:00:32 -0500

    add one more test

--- distinctListPython/distinctList.py
+++ distinctListPython/distinctList.py
@@ -8,12 +8,12 @@ def DistinctList(arr):
       if arr.count(val) > 1 and start != val:
           count += (arr.count(val) - 1)
           start = val
-      else:
-          next
+    #   else:
+    #       next
   return count
 
 print(DistinctList([0,-2,-2,5,5,5]))    #answer is 3
 print(DistinctList([100,2,101,4]))      #answer is 0
-
+print(DistinctList([1, 2, 2, 2, 3]))    #answer is 2
 # for item in arr:
 # arr.count(args)
