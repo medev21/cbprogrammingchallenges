db0aeb2b92e88791a3baa47657a497c8a583c2ab
Martin Ben <mebenav88@gmail.com>
2015-11-02 22:42:04 -0500

    add sort method to array

---
+++ distinctListPython/distinctList.py
@@ -0,0 +1,10 @@
+def DistinctList(arr):
+
+  # code goes here
+  arr = sorted(arr)
+  count = 0
+  start = arr[0]
+  return arr
+
+print(DistinctList([0,-2,-2,5,5,5]))    #answer is 3
+print(DistinctList([100,2,101,4]))
