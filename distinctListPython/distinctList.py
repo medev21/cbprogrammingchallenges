2be046f71282fa4b4a1a1717fffbf175512fa51e
Martin Ben <mebenav88@gmail.com>
2015-11-02 22:58:07 -0500

    add for loop

--- distinctListPython/distinctList.py
+++ distinctListPython/distinctList.py
@@ -3,8 +3,17 @@ def DistinctList(arr):
   # code goes here
   arr = sorted(arr)
   count = 0
-  start = arr[0]
-  return arr
+  start = 0
+  for val in arr:
+      if arr.count(val) > 1 and start != val:
+          count += (arr.count(val) - 1)
+          start = val
+      else:
+          next
+  return count
 
 print(DistinctList([0,-2,-2,5,5,5]))    #answer is 3
-print(DistinctList([100,2,101,4]))
+print(DistinctList([100,2,101,4]))      #answer is 0
+
+# for item in arr:
+# arr.count(args)
