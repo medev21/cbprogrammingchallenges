6c3adc5ef7bb6aa613ac12d2439f2e4fc307bbec
Martin Ben <mebenav88@gmail.com>
2015-11-06 14:57:31 -0500

    add each loop with paramaeter

--- lookSaySequenceRuby/lookSaySequence.rb
+++ lookSaySequenceRuby/lookSaySequence.rb
@@ -3,7 +3,15 @@ def LookSaySequence(num)
   # code goes here
   arr = num.to_s.split('')
   arr.each {|x| x.to_i}
-  return arr
+  res = []
+  prev = 0
+  arr.each do |i|
+    count = i
+    num = i
+
+    res << count << i
+  end
+  return res
 
 end
 
