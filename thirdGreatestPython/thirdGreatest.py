74799cfc27a08e929050dedecfcc811b40b7fd0f
Martin Ben <mebenav88@gmail.com>
2015-11-11 23:53:08 -0500

    add another loop to get string with largest length

--- thirdGreatestPython/thirdGreatest.py
+++ thirdGreatestPython/thirdGreatest.py
@@ -4,11 +4,20 @@ def ThirdGreatest(strArr):
   large_words = []
   size = 0
   for val in strArr:
-      if len(val) > size:
-          large_words.insert(0, val)
-          strArr.remove(val)
-      else:
+      if len(val) >= size:
           size = len(val)
+
+  print(size)
+
+  while True:
+      for val in strArr:
+          if len(val) >= size:
+              large_words.insert(0, val)
+              strArr.remove(val)
+              print(strArr)
+          else:
+              size = len(val)
   return large_words
 
-print(ThirdGreatest(["coder","byte","code"]))
+# print(ThirdGreatest(["coder","byte","code"]))
+print(ThirdGreatest(["abc","defg","z","hijk"]))
