436ff6c47f6ea857cdffbdc6d07e6f211be2b5f4
Martin Ben <mebenav88@gmail.com>
2015-11-12 00:37:13 -0500

    add while loop

--- thirdGreatestPython/thirdGreatest.py
+++ thirdGreatestPython/thirdGreatest.py
@@ -6,18 +6,23 @@ def ThirdGreatest(strArr):
   for val in strArr:
       if len(val) >= size:
           size = len(val)
-
-  print(size)
-
-  while True:
+  
+  i = 0
+  while i < 3:
       for val in strArr:
-          if len(val) >= size:
+          if size == len(val):
               large_words.insert(0, val)
               strArr.remove(val)
-              print(strArr)
-          else:
+              size = 0
+            #   print(strArr)
+
+      for val in strArr:
+          if len(val) >= size:
               size = len(val)
+            #   print(size)
+      i = i + 1
   return large_words
 
-# print(ThirdGreatest(["coder","byte","code"]))
-print(ThirdGreatest(["abc","defg","z","hijk"]))
+print(ThirdGreatest(["coder","byte","code"])) #code
+print(ThirdGreatest(["abc","defg","z","hijk"])) #abc
+print(ThirdGreatest(["hello", "world", "before", "all"]))   #world
